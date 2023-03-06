4bbd0cc5534cbf0e73554e13bf3026488fbbc4f4
Antônio Della Flora <antoniodellaflora@gmail.com>
2023-03-06 07:51:40 -0300

    ajustes para compilar multiplos arquivos

--- projetoFinal/main.c
+++ projetoFinal/main.c
@@ -20,6 +20,8 @@
  ********************************************************************************************/
 
 #include "raylib.h"
+#include "tad.h"
+#include "teste2.h"
 
 #if defined(PLATFORM_WEB)
 #include <emscripten/emscripten.h>
@@ -52,6 +54,9 @@ int main() {
   camera.up = (Vector3){0.0f, 1.0f, 0.0f};
   camera.fovy = 60.0f;
   camera.projection = CAMERA_PERSPECTIVE;
+
+  teste();
+  blaaa();
 
   SetCameraMode(camera, CAMERA_ORBITAL);
 
