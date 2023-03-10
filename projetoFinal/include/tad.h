798177e872908c0707ffc9aece0917ee5ce702ea
Antônio Della Flora <antoniodellaflora@gmail.com>
2023-03-10 09:23:41 -0300

    tad genérico para o baralho

--- projetoFinal/include/tad.h
+++ projetoFinal/include/tad.h
@@ -1,1 +1,24 @@
-void teste();
+#ifndef _TAD_H_
+#define _TAD_H_
+
+typedef struct nodoLGEnc {
+  void *info;
+  struct nodoLGEnc *prox;
+} NodoLGEnc;
+
+typedef struct {
+  NodoLGEnc *prim;
+} ListaGEnc;
+
+// Funcao que cria uma lista generica
+ListaGEnc *criaListaGEnc();
+
+// Funcao que destroi uma lista generica
+// void destroiListaGEnc(ListaGEnc *lista); // TODO: Codar isso
+
+int insereInicioListaGEnc(ListaGEnc *lista, void *info);
+
+// Percorre a lista
+void percorreListaGEnc(ListaGEnc *lista, void (*cb)(void *));
+
+#endif
